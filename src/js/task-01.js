e70862c5e2b5ff54853307914c2ac8181c879093
Maryna <marynaskyba@gmail.com>
2021-06-03 11:44:49 +0200

    add task 2

---
+++ src/js/task-01.js
@@ -0,0 +1,23 @@
+
+const navListLink = document.querySelector('.item');
+console.log(navListLink);
+
+
+
+const firstEl = navListLink.firstElementChild.textContent.forEach;
+console.log(firstEl);
+
+
+
+// const itemLength = navListLink.length;
+// console.log(itemLength);
+
+// console.log("У списку ${itemLength} категорії");
+
+
+// const textEl = navListLink.textContent;
+// console.log(textEl);
+
+
+
+// console.log('Категорія:', navListLink.textContent, 'Кількість елементів:',  );
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map((ingredient) => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+
+  return itemEl;
+
+});
+
+console.log(elements);
+
+ingredientEl.append(...elements);
+
+
