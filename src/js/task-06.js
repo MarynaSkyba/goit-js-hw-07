142e387a0d519997c77e5ce1da8025a9872b7a22
Maryna <marynaskyba@gmail.com>
2021-06-04 13:53:57 +0200

    update task 06

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,10 +1,11 @@
 const inputEl = document.querySelector('#validation-input');
-inputEl.addEventListener('focus', (event) =>{
-    inputEl.style.display = 'block';
-})
 
-inputEl.addEventListener('blur', (event) =>{
+inputEl.addEventListener('blur', (event) => {
    if(event.currentTarget.value.length <= 6) {
        event.currentTarget.classList.add('invalid')
-   }
+   } else {
+       if (event.currentTarget.value.length > 6){
+    event.currentTarget.classList.remove('invalid');
+    event.currentTarget.classList.add('valid')
+   }}
 })
