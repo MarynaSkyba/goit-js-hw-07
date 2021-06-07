fbdc8c42383c5618d267a4f8852700228038d0ff
Maryna <marynaskyba@gmail.com>
2021-06-07 20:18:42 +0200

    update task 05

--- js/task-05.js
+++ js/task-05.js
@@ -2,18 +2,14 @@ const inputName = document.querySelector('#name-input');
 const spanName = document.querySelector('#name-output');
 
 inputName.addEventListener('input', (event) => {
-spanName.textContent = event.currentTarget.value.trim() || 'незнакомец' })
+// spanName.textContent = event.currentTarget.value.trim() || 'незнакомец' })
 
 
-// if (event.currentTarget.value !== '') {  
-//     spanName.textContent = event.currentTarget.value.trim();
-// }
-//     else {
-//    return 'незнакомец';
-// }}
-
-
-
-
+if (event.currentTarget.value.trim()) {  
+    spanName.textContent = event.currentTarget.value;
+}
+    else {
+        spanName.textContent = 'незнакомец';
+    }});
 
 
